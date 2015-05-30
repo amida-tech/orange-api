4005ff291978e6abd2ec8de7ffccb581bd27e178
Harry Rickards <rickards@mit.edu>
2015-05-30 04:30:09 -0400

    Stub out rest of aglio docs

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat src/api.md src/authentication.md src/user.md src/habits.md > tmp.md
+cat src/api.md src/authentication.md src/user.md src/habits.md src/doctors.md src/pharmacies.md src/medications.md src/adherences.md src/shared.md > tmp.md
 aglio -i tmp.md --theme flatly -o output/index.html
 rm -f tmp.md
