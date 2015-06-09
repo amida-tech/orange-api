5e9d03a24e625b257cbd1e379e76a2e4e258b661
Harry Rickards <rickards@mit.edu>
2015-06-09 16:14:59 -0400

    Finish medications CRUD

---
+++ test/medications/delete_medication_test.js
@@ -0,0 +1,16 @@
+"use strict";
+var util            = require("util"),
+    async           = require("async"),
+    factories       = require("../common/factories.js"),
+    requests        = require("../common/requests.js"),
+    Crud            = require("../common/crud.js"),
+    auth            = require("../common/auth.js"),
+    patients        = require("../patients/common.js"),
+    PatientsCrud    = require("../common/patients_crud.js");
+
+var crud = new PatientsCrud("Medication", "medication", "medications");
+var keys = ["id", "name", "rx_norm", "ndc", "dose", "route", "form", "rx_number", "quantity", "type", "schedule", "doctor_id", "pharmacy_id"];
+
+describe("delete a medication (DELETE /patients/:patientid/medications/:medicationid)", function () {
+    crud.deletes(keys);
+});
