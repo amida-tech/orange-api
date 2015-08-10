f7e5a0ee405cfdd4093bec8a0dc1e16e10f3fa59
Harry Rickards <rickards@mit.edu>
2015-08-10 14:04:38 -0400

    Linting

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ app.use(logger);
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Client-Secret");
+    res.header("Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Client-Secret");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
