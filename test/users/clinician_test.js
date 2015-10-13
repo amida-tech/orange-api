bca216900b05117816cf257864dd6e7d37bf03fb
Harry Rickards <rickards@mit.edu>
2015-10-12 22:38:57 -0400

    Test clincian has full read access

---
+++ test/users/clinician_test.js
@@ -0,0 +1,59 @@
+"use strict";
+var chakram         = require("chakram"),
+    Q               = require("q"),
+    auth            = require("../common/auth.js"),
+    patients        = require("../patients/common.js"),
+    showMedication  = require("../medications/view_medication_test.js").show;
+
+var expect = chakram.expect;
+
+describe("Users", function () {
+    describe("Clinician Access", function () {
+        // setup a test non-clinician user with
+        var user;
+        before(function () {
+            return auth.createTestUser().then(function (u) {
+                user = u;
+            });
+        });
+
+        // setup a test patient for that user
+        var patient;
+        before(function () {
+            return patients.createMyPatient({}, user).then(function (p) {
+                patient = p;
+            });
+        });
+
+        // setup a test medication owned by the patient
+        var medication;
+        before(function () {
+            var create = Q.nbind(patient.createMedication, patient);
+            return create({
+                name: "foobar"
+            }).then(function (m) {
+                medication = m;
+            });
+        });
+
+
+        // setup a test clinician user
+        var clinician;
+        before(function () {
+            return auth.createTestUser({
+                clinician: true
+            }).then(function (u) {
+                clinician = u;
+            });
+        });
+
+        it("grants access to the patient", function () {
+            return expect(patients.show(patient._id, clinician.accessToken)).to.be.a.patient.success;
+        });
+
+        it("grants access to the medication", function () {
+            var endpoint = showMedication(medication._id, patient._id, clinician.accessToken);
+            return expect(endpoint).to.be.a.medication.viewSuccess;
+        });
+    });
+});
