50273f48abaf814d080e2f0c685623a76b12c8aa
Elijah George <elijah@amida.com>
2018-02-16 16:16:41 -0500

    clean up

--- gruntfile.js
+++ gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
                     // bail: true,
                     timeout: "10000"
                 },
-                src: ["test/common/db_helper.js", "test/common/*.js", "test/*.js", "test/*/common.js", "test/patients/create_new_patient_test.js"]
+                src: ["test/common/db_helper.js", "test/common/*.js", "test/*.js", "test/*/common.js", "test/**/*.js"]
             },
             unit: {
                 options: {
