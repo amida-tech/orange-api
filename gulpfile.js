b0ef29cbef69f1912d9b8b3fecc871ea208653d7
Elijah George <elijah@amida.com>
2018-02-19 15:32:21 -0500

    update code analysis to use project .eslint file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require('gulp');
 let plato = require('es6-plato');
+let fs = require("fs");
 
 let appSrc = './lib/**';
 let testSrc = './test/**';
@@ -7,47 +8,10 @@ let testSrc = './test/**';
 let appOutputDir = './artifacts/lib';
 let testOutputDir = './artifacts/test';
 
+let complexityRules = {};
 
-let lintRules = {
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "script"
-    },
-    "env": {
-        "node": true,
-        "mocha": true
-    },
-    "rules": {
-        "curly": 0,
-        "key-spacing": 1,
-        "no-shadow": 0,
-        "no-underscore-dangle": 0,
-        "camelcase": 0,
-        "no-use-before-define": [2, 'nofunc'],
-        "max-len": [1, 120, 4],
-        "new-cap": 0,
-        "no-unused-expressions": 0,
-        "smells/no-switch": 1,
-        "smells/no-complex-switch-case": 1,
-        "smells/no-setinterval": 1,
-        "smells/no-complex-string-concat": 1,
-        "no-multi-spaces": [2, {
-            "exceptions": {
-                "VariableDeclarator": true
-            }
-        }],
-        "no-unused-vars": 1
-    },
-    "parser": "babel-eslint",
-    "plugins": [
-        "smells"
-    ]
-};
-
-
-let complexityRules = {
-
-};
+let data = fs.readFileSync('.eslintrc');
+let lintRules = JSON.parse(data);
 
 let platoArgs = {
     title: 'Orange-API',
