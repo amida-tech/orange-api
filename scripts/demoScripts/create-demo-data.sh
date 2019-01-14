ff11b38dd171778ad63a308593f1b6d6a02e1784
Aaron Houlihan <aaron@amida.com>
2019-01-14 14:37:05 -0500

    big changes to Dockerfile. add create-demo-data.sh.

---
+++ scripts/demoScripts/create-demo-data.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# ORANGE_DEMO_CLINICIAN_EMAIL
+# ORANGE_DEMO_CLINICIAN_PASSWORD
+# ORANGE_DEMO_PATIENTS=['patient_one', 'patient_two']
+
+# ./reportsMockScript.js
+
+echo "hi hi hi hi hi";
+
+# curl -s "http://api.icndb.com/jokes/random" | jq '.value.joke'
+
+# {
+#   "clinician": {
+#     "email": "scott+democlinician@verizon.net"
+#     "password": "Test123!"
+#   },
+#   "patients": [
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#     {
+#       "email": "demopatient1@amida.com",
+#       "password": "Test123!"
+#     },
+#   ]
+# }
